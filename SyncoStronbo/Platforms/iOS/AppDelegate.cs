a9da9a01c01577c392420c3f8e2640f8895d2f8f
Florian Lebecque <florianlebecque@hotmail.com>
2026-03-14 13:11:51 +0000

    remove IOS/MAC Add dev container, Add android simulator

--- SyncoStronbo/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace SyncoStronbo {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
-}
